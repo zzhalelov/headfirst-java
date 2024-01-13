e51fcdc8338a0eb2947b1acdc2c2cabb2369a99c
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-01-13 23:39:59 +0600

    Task 2. fixed

--- src/chapter_01/task02/Main.java
+++ src/chapter_01/task02/Main.java
@@ -1,2 +1,32 @@
-package chapter_01.task02;public class Main {
+package chapter_01.task02;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 0;
+        while (x < 5) {
+            y = x - y;
+
+            y = y + x;
+
+            y = y + 2;
+            if (y > 4) {
+                y = y - 1;
+            }
+
+            x = x + 1;
+            y = y + x;
+
+            if (y < 5) {
+                x = x + 1;
+                if (y < 3) {
+                    x = x - 1;
+                }
+            }
+            y = y + 2;
+
+            System.out.print(x + "" + y + " ");
+            x = x + 1;
+        }
+    }
 }
