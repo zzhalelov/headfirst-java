aa1a91bd150d6847b68c4c70dfa59d98a5ed5408
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-01-14 00:54:02 +0600

    Chapter 02. Task03

---
+++ src/chapter_02/task03/DrumKit.java
@@ -0,0 +1,14 @@
+package chapter_02.task03;
+
+public class DrumKit {
+    boolean topHat = true;
+    boolean snare = true;
+
+    void playSnare() {
+        System.out.println("бах бах ба-бах");
+    }
+
+    void playTopHat() {
+        System.out.println("динь динь ди-динь");
+    }
+}
---
+++ src/chapter_02/task03/DrumKitTestDrive.java
@@ -0,0 +1,9 @@
+package chapter_02.task03;
+
+public class DrumKitTestDrive {
+    public static void main(String[] args) {
+        DrumKit d = new DrumKit();
+        d.playSnare();
+        d.playTopHat();
+    }
+}
