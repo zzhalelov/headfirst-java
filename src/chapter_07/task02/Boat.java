be3b45e2b2b378ef4a0312f52534b0030bf949e3
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-02-14 23:48:17 +0600

    feat: Chapter 07. Task 02. Add class Boat.java

---
+++ src/chapter_07/task02/Boat.java
@@ -0,0 +1,17 @@
+package chapter_07.task02;
+
+public class Boat {
+    private int length;
+
+    public void setLength(int len) {
+        length = len;
+    }
+
+    public void getLength() {
+        return;
+    }
+
+    public void move() {
+        System.out.println("Дрейф");
+    }
+}
